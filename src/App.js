467c5b70bf4ea20b29d2ca74ad5f777ceaddac90
sedthawuth.m <sedthawuth.m@lmwn.com>
2023-06-04 17:37:25 +0700

    try json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/"
+      )
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
   return (
     <div className='App'>
       <Link to='/about'>About</Link>
       <h2>Github Pages</h2>
       <h3>Deploying React to Github</h3>
+      {data.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
     </div>
   )
 }
